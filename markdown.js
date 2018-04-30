54908e41c3699e85de7bede74966c67a65a8735f
BHess2653 <GM_Dahaka@BHMac.local>
2018-04-30 16:32:03 -0400

    added h2-h6 working on next items list, link, and code blocks

--- markdown.js
+++ markdown.js
@@ -2,7 +2,12 @@ const util = require('./src/tool');
 const fs = require('fs');
 
 const dataset = [
-  { type: 'h1', value: 'Hello World'}
+  { type: 'h1', value: 'H1 Hello'},
+  { type: 'h2', value: 'H2 Hello'},
+  { type: 'h3', value: 'H3 Hello'},
+  { type: 'h4', value: 'H4 Hello'},
+  { type: 'h5', value: 'H5 Hello'},
+  { type: 'h6', value: 'H6 Hello'},
 ];
 
 let markdown = [];
@@ -13,6 +18,21 @@ const builder = function(data) {
 
     if (type == "h1") {
       markdown += `# ${value} `;
+    }
+    if (type == "h2") {
+      markdown += `## ${value} `;
+    }
+    if (type == "h3") {
+      markdown += `### ${value} `;
+    }
+    if (type == "h4") {
+      markdown += `#### ${value} `;
+    }
+    if (type == "h5") {
+      markdown += `##### ${value} `;
+    }
+    if (type == "h6") {
+      markdown += `###### ${value} `;
     }
   });
   util.log('My Markdown \r\n', markdown);
