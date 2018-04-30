deded9b9e66c2bfa1ad6af622a6e76c63412e3de
Matt <mattpezzente@gmail.com>
2018-04-30 14:46:44 -0400

    App.js in progress

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const { formGenerator } = require('./tool.js')
+const inputs = [
+  {
+    type: 'text' 
+  }, {
+    type: 'email'
+  }, {
+    type: 'number'
+  }, {
+    type: 'submit'
+  }
+]
+
+http.createServer(function(request, response) {  
+  response.writeHeader(200, {"Content-Type": "text/html"});  
+  response.write(formGenerator(inputs));
+  response.end();  
+}).listen(8000);
--- src/tool.js
+++ src/tool.js
@@ -2,25 +2,13 @@ module.exports.formBuilder = (inputs) => {
   let html = ''
 
   for (let i = 0; i < inputs.length; i++) {
-    switch(inputs[i].type) {
-      case 'text':
-        html += ('<input type="text"></input>')
-        break;
-      case 'select':
-        html += ('<input type="select"></input>')
-        break;
-      case 'number':
-        html += ('<input type="number"></input>')
-        break;
-      case 'radio':
-        html += ('<input type="radio"></input>')
-        break;
-      case 'email':
-        html += ('<input type="email"></input>')
-        break;
-      case 'button':
-        html += ('<button type="button">Click</button>')
-        break;
+    if (
+      inputs[i].type === 'text' ||
+      inputs[i].type === 'email' ||
+      inputs[i].type === 'number' ||
+      inputs[i].type === 'submit'
+    ) {
+      html += `<input type="${inputs[i].type}"></input>`
     }
   }
 
