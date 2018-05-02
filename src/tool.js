5c03012ad179344ca02a871654d7c0b4a0116db9
BHess2653 <GM_Dahaka@BHMac.local>
2018-05-01 23:10:47 -0400

    changed == to === and added module. at the top of tool.js

--- src/tool.js
+++ src/tool.js
@@ -1,4 +1,4 @@
-exports.builder = (markdownInput) => {
+module.exports.builder = (markdownInput) => {
 
   let markdown = [];
 
@@ -6,40 +6,40 @@ exports.builder = (markdownInput) => {
     const { type, text, link, code } = inputData;
     const nl = '\n'; // New Line
 
-    if (type == "title") {
+    if (type === "title") {
       markdown += nl + `## ${text} ` + nl;
     }
-    if (type == "h1") {
+    if (type === "h1") {
       markdown += nl + `# ${text} ` + nl;
     }
-    if (type == "h2") {
+    if (type === "h2") {
       markdown += nl + `## ${text} ` + nl;
     }
-    if (type == "h3") {
+    if (type === "h3") {
       markdown += nl + `### ${text} ` + nl;
     }
-    if (type == "h4") {
+    if (type === "h4") {
       markdown += nl + `#### ${text} ` + nl;
     }
-    if (type == "h5") {
+    if (type === "h5") {
       markdown += nl + `##### ${text} ` + nl;
     }
-    if (type == "h6") {
+    if (type === "h6") {
       markdown += nl + `###### ${text} ` + nl;
     }
-    if (type == "list") {
+    if (type === "list") {
       markdown += nl + `${text} ` + nl;
     }
-    if (type == "link") {
+    if (type === "link") {
       markdown += nl + `[${link.text}](${link.url})` + nl;
     }
-    if (type == "code_js") {
+    if (type === "code_js") {
       markdown += nl + '```js' + `${code}` + '```' + nl;
     }
-    if (type == "code_css") {
+    if (type === "code_css") {
       markdown += nl + '```css' + `${code}` + '```' + nl;
     }
-    if (type == "code_md") {
+    if (type === "code_md") {
       markdown += nl + '```md' + `${code}` + '```' + nl;
     }
   });
